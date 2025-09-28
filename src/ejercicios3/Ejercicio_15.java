cff2c9d9f7ad77d23623c948c6587409e83ab13c
alvar <alvarodaca@outlook.es>
2025-09-28 21:59:48 +0200

    1.3

---
+++ src/ejercicios3/Ejercicio_15.java
@@ -0,0 +1,23 @@
+package ejercicios3;
+
+/*
+Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente traducción al castellano.
+Las palabras deben estar distribuidas en dos columnas y alineadas a la izquierda. Pista: Se puede insertar un tabulador mediante \t.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio_15 {
+    public static void main(String[] args) {
+        System.out.println("grey\tgris");
+        System.out.println("pencil\tlapiz");
+        System.out.println("bottle\tbotella");
+        System.out.println("number\tnumero");
+        System.out.println("green\tverde");
+        System.out.println("laptop\tportatil");
+        System.out.println("phone\ttelefono");
+        System.out.println("mouse\traton");
+        System.out.println("paper\tpapel");
+        System.out.println("book\tlibro");
+    }
+}
---
+++ src/ejercicios3/Ejercicio_16.java
@@ -0,0 +1,28 @@
+package ejercicios3;
+
+/*
+Mostrar la tabla de multiplicar de un número.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio_16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserte un número: ");
+
+        int numero = scanner.nextInt();
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+
+        System.out.println(numero);
+        System.out.println(numero + numero);
+        System.out.println(numero + numero + numero);
+        System.out.println(numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero + numero + numero + numero + numero);
+        System.out.println(numero + numero + numero + numero + numero + numero + numero + numero + numero + numero);
+    }
+}
