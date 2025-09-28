724cd057bc83b950502c7409205fb1c10959487a
alvar <alvarodaca@outlook.es>
2025-09-28 02:52:40 +0200

    1.3

---
+++ src/ejercicios3/Ejercicio_7.java
@@ -0,0 +1,17 @@
+package ejercicios3;
+
+/*
+Programa que lea un carácter por teclado y compruebe si es una letra mayúscula.
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un caracter: ");
+
+        char caracter = scanner.nextLine().charAt(0);
+        System.out.println((caracter >= 'A' && caracter <= 'Z') ? "Es mayuscula" : "Es minuscula");
+    }
+}
