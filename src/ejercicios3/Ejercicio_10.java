ee20099523a70ede6e1a76c923c01b7f8f3c60c2
alvar <alvarodaca@outlook.es>
2025-09-28 18:56:49 +0200

    1.3

---
+++ src/ejercicios3/Ejercicio_10.java
@@ -0,0 +1,23 @@
+package ejercicios3;
+
+/*
+Desarrolla un programa que solicite 3 números y compruebe si están ordenados.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+
+        int numero = scanner.nextInt();
+        System.out.println("Ingrese un número: ");
+
+        int num = scanner.nextInt();
+        System.out.println("Ingrese un número: ");
+
+        int n = scanner.nextInt();
+        System.out.println((numero >= num && num >= n) || (numero <= num && num <= n) ? "Estan ordenados" : "No estan ordenados");
+    }
+}
