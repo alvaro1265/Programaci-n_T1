2aa20b4fc565eeb1c73c3b852533d3bfde72c1b0
alvar <alvarodaca@outlook.es>
2025-10-08 00:00:01 +0200

    2.1

---
+++ src/UT2/Ejercicios1/Ejercicio_9.java
@@ -0,0 +1,15 @@
+package UT2.Ejercicios1;
+
+/*
+Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+*/
+
+public class Ejercicio_9 {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 100) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
