4cffb983a39b02d8608de5d4e3d33dd36dc5944b
DCA <po@>
2025-10-27 20:35:25 +0100

    factorial

---
+++ src/Recursiva/FactorialRecursivo.java
@@ -0,0 +1,17 @@
+package Recursiva;
+
+public class FactorialRecursivo {
+
+    public static void main(String[] args) {
+        int factorial5 = factorial(5);
+        System.out.println("El factorial de 5 es " + factorial5);
+    }
+
+    static int factorial(int numero) {
+        if (numero == 1) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
+}
