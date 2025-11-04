b44f6737f5390eb28c6486fc3865a4d3aadf3671
DCA <po@>
2025-11-04 19:54:16 +0100

    2.4

--- src/UT2/Ejercicios4/Ejercicio_4.java
+++ src/UT2/Ejercicios4/Ejercicio_4.java
@@ -1,7 +1,8 @@
 package UT2.Ejercicios4;
 
 /*
-Realiza un metodo redondear() al que se le pasa como parámetro un numero float y los decimales que deben quedar después de redondear, y retorna el numero redondeado.
+Realiza un metodo redondear() al que se le pasa como parámetro un numero float
+y los decimales que deben quedar después de redondear, y retorna el numero redondeado.
 Ejemplo: redondear( 234.49234 , 3 )
 Da como resultado : 234.492
 */
@@ -11,17 +12,25 @@ import java.util.Scanner;
 public class Ejercicio_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ingresa un numero con decimales: ");
         float numero = scanner.nextFloat();
+
+        System.out.println("ingresa el numero de dcimales que se requieran:");
+        int decimales = scanner.nextInt();
+
         scanner.close();
 
-    }
-    /*
-    static float redondear(float a, int n) {
-        if () {
+        float resultado = redondear(numero, decimales);
+        System.out.println(" " + resultado);
 
+    }
+
+    static float redondear(float a, int n) {
+        if (a == 0) {
+            return a;
+        } else {
+            return Math.round((double) n);
         }
     }
-
-     */
 }
