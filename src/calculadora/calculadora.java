092f45a4f6d562fa0570f0dd3c7003aa13833b6e
DCA <po@>
2025-11-05 17:03:42 +0100

    calculadora

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class calculadora {
+    public static void main(String[] args) {
+
+    }
+    static void calculadora() {
+
+    }
+}
---
+++ src/calculadora/calculadoraTest.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculadora() {
+    }
+}
