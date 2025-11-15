22bbfb9c7e1cd06f8a13de844d539fe57392704f
alvar <alvarodaca@outlook.es>
2025-11-15 03:01:53 +0100

    calcula

--- src/calculadora/calculadoraTest.java
+++ src/calculadora/calculadoraTest.java
@@ -1,14 +1,14 @@
 package calculadora;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class calculadoraTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
-    }
+   // @org.junit.jupiter.api.Test
+   // void main() {
+   // }
 
-    @org.junit.jupiter.api.Test
-    void calculadora() {
-    }
+   // @org.junit.jupiter.api.Test
+    //void calculadora() {
+    //}
 }
