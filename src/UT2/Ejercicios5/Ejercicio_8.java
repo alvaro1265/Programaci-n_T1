5a1a6bf7c2333f0646125250c1bdf8bd79ba3a2d
alvar <alvarodaca@outlook.es>
2025-11-18 13:44:01 +0100

    2.5

---
+++ src/UT2/Ejercicios5/Ejercicio_8.java
@@ -0,0 +1,12 @@
+package UT2.Ejercicios5;
+
+/*
+Comprobar si un array de 5x5 es un cuadro mágico.
+Se considera un cuadro mágico aquel en el que las filas, las columnas y las diagonales suman igual.
+*/
+
+public class Ejercicio_8 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/UT2/Ejercicios5/Ejercicio_9.java
@@ -0,0 +1,12 @@
+package UT2.Ejercicios5;
+
+/*
+Crea un programa que mezcle dos arrays ordenados de N elementos,
+consiguiendo otro array de N*2 elementos igualmente ordenados.
+*/
+
+public class Ejercicio_9 {
+    public static void main(String[] args) {
+
+    }
+}
